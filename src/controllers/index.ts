4d54f01b4c0b743e9f9a8f32b6387fad7bb88428
TonyBSosa <tonyblad09@gmail.com>
2022-12-07 12:29:26 -0600

    proyecto base de backend vacaciones

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './empleado.controller';
+export * from './cargo.controller';
+export * from './codigo-trabajo.controller';
+export * from './cargos.controller';
+export * from './vacaciones.controller';
